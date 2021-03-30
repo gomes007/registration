f682e00c1f26633b4fedb922227a3572d32eac31
Paulo Gomes <gomes7.paulo@gmail.com>
2021-03-30 13:05:25 -0300

    solucao problema servidor.

--- Registration/src/br/com/project/listener/ContextLoaderListenerCaixakiUtils.java
+++ Registration/src/br/com/project/listener/ContextLoaderListenerCaixakiUtils.java
@@ -4,17 +4,18 @@ import java.io.Serializable;
 
 import javax.faces.bean.ApplicationScoped;
 
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 @ApplicationScoped
-public class ContextLoaderListenerCaixakiUtils extends	org.springframework.web.context.ContextLoaderListener implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
+public class ContextLoaderListenerCaixakiUtils extends ContextLoaderListener implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
+	
 	private static WebApplicationContext getWac() {
-		return WebApplicationContextUtils
-				.getWebApplicationContext(getCurrentWebApplicationContext().getServletContext());
+		return WebApplicationContextUtils.getWebApplicationContext(getCurrentWebApplicationContext().getServletContext());		
 	}
 	
 	public static Object getBean(String idNomeBean) {
